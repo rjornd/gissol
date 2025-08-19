cf5150e67c055994c72c73b9d158295a9ceff3a4
domrachev_ke <rjornd@yandex.ru>
2025-08-19 19:29:50 +0500

    settings

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/gissol/', 
+  base: '/', 
   plugins: [react()],
-  assetsInclude: ['**/*.glb']
+  assetsInclude: ['**/*.docx']
 })
